a945fc117325497483d56202cdff88db32a42066
Alex Koshelev <daevaorn@gmail.com>
2010-03-28 01:02:55 +0300

    oops. added missing backend object creation

--- django_dust/__init__.py
+++ django_dust/__init__.py
@@ -1,4 +1,5 @@
 from django.conf import settings
 from django.utils.importlib import import_module
 
-retry_backend = import_module(settings.DUST_RETRY_STORAGE_BACKEND)
+backend_module = import_module(settings.DUST_RETRY_STORAGE_BACKEND)
+retry_backend = backend_module.RetryStorage()
