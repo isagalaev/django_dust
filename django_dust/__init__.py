0afbc346f98786de8adda8da352337a153fe1634
Alex Koshelev <daevaorn@gmail.com>
2010-03-29 01:47:45 +0400

    fixed global storage backend instance name

--- django_dust/__init__.py
+++ django_dust/__init__.py
@@ -2,4 +2,4 @@ from django.conf import settings
 from django.utils.importlib import import_module
 
 backend_module = import_module(settings.DUST_RETRY_STORAGE_BACKEND)
-retry_backend = backend_module.RetryStorage()
+retry_storage = backend_module.RetryStorage()
