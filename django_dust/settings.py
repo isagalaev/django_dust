18684fdedff13d36ec8a357db748416713b5a553
Alex Koshelev <daevaorn@gmail.com>
2010-03-29 02:02:56 +0400

    added more default settings

--- django_dust/settings.py
+++ django_dust/settings.py
@@ -2,3 +2,9 @@
 # Retry storage backend setting -- import path for storage module
 # Default is DB backend
 DUST_RETRY_STORAGE_BACKEND = 'django_dust.backends.db'
+
+# Timeout for http operations
+DUST_TIMEOUT = 2
+
+# Hosts list to distribute files to
+DUST_HOSTS = []
