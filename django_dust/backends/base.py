7d5eb85ffe0c233ea5dcc72a7d9f628693c620e8
Alex Koshelev <daevaorn@gmail.com>
2010-03-29 08:55:55 +0400

    added doc strings to base retry storage methods

--- django_dust/backends/base.py
+++ django_dust/backends/base.py
@@ -3,16 +3,21 @@ class BaseRetryStorage(object):
     fields = ['operation', 'target_host', 'source_host', 'filename']
     
     def count(self):
+        """Returs total retry count"""
         raise NotImplementedError
 
     def all(self):
+        """Returns all retries in queue"""
         raise NotImplementedError
 
     def create(self, **kwargs):
+        """Creates new retry object in queue"""
         raise NotImplementedError
 
     def delete(self, retry):
+        """Deletes given retry object from queue"""
         raise NotImplementedError
 
     def filter_by_filename(self, filename):
+        """Returns retry objects for given file name"""
         raise NotImplementedError
