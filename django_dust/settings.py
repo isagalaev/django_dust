70528ab419a85fe6d6d714bde68ffd38efe738f1
Alex Koshelev <daevaorn@gmail.com>
2010-03-30 09:12:20 +0400

    Fixed default settings description

--- django_dust/settings.py
+++ django_dust/settings.py
@@ -3,8 +3,14 @@
 # Default is DB backend
 DUST_RETRY_STORAGE_BACKEND = 'django_dust.backends.db'
 
-# Timeout for http operations
+# Timeout in seconds for accessing hosts over network. Defaults to 2.
 DUST_TIMEOUT = 2
 
-# Hosts list to distribute files to
+# List of file storage hosts
 DUST_HOSTS = []
+
+# Whether to use a local file system. If files are stored on those same
+# servers that handle web requests then setting this flag to True
+# allows django_dust to access files locally saving time and bandwidth
+# on existence checking operations. Defaults to False.
+DUST_USE_LOCAL_FS = True
